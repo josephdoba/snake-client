16d407f369b7e13a2fc365740e9998f415bdd79a
Joseph Doba <josephdoba@gmail.com>
2022-04-22 11:32:39 -0700

    experiemented with the move commands

--- client.js
+++ client.js
@@ -12,8 +12,22 @@ const connect = function() {
   conn.on("connect", () => {
     conn.write('Name: SNK');
     console.log("Successfully connected to game server");
+    // Move command tests:
+    // setInterval(() => {
+    //   conn.write('Move: down');
+    // }, 250);
+    // setInterval(() => {
+    //   conn.write('Move: right');
+    // }, 750);
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 1250);
+    // setInterval(() => {
+    //   conn.write("Move: left");
+    // }, 1750);
 
   });
+
   // when there is no input for 5 seconds, disconnect the client from the server
   conn.setTimeout(5000, ()=> {
     console.log(`You afk'd for 5 seconds so you lost. Git gud scrub`);
--- play.js
+++ play.js
@@ -1,6 +1,5 @@
 // import functions from client.js
 const connect = require('./client');
-
 // run the connection:
 console.log("Connecting ...");
 connect();
